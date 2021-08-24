3da365e6fff877700c6c7bec4c5c2d6d078d2c50
Алексей <anonymous.aleksey@gmail.com>
2021-08-24 11:36:00 +0300

    done users filter

--- frontend/controllers/UsersController.php
+++ frontend/controllers/UsersController.php
@@ -2,19 +2,55 @@
 
 namespace frontend\controllers;
 
+use frontend\models\Categories;
+use frontend\models\FilterUsers;
 use frontend\models\Responds;
 use frontend\models\Reviews;
 use frontend\models\Users;
+use Yii;
 use yii\db\Query;
+use yii\helpers\ArrayHelper;
 
 class UsersController extends \yii\web\Controller
 {
+    public $users;
     public function actionIndex()
     {
-        $users = Users::find()->where(['role' => Users::ROLE_EXECUTOR])
-            ->with('executorCategories.category')->with('executeTasks.reviews')
-            ->orderBy('dt_add DESC')->all();
+           $get = Yii::$app->request->get();
+           $model = new FilterUsers();
+           $model->load($get);
 
-        return $this->render('index', ['users' => $users]);
+        $categories = Categories::find()->all();
+        $this->users = Users::find()->where(['role' => Users::ROLE_EXECUTOR])
+            ->with('executorCategories.category')->with('executeTasks.reviews')
+            ->joinWith('executeTasks')
+            ->joinWith('executorCategories')
+            ->orderBy('dt_add DESC');
+        if ($model->load(Yii::$app->request->get())) {
+            if ($model->search) {
+                $model->options = NULL;
+                $model->category_id = NULL;
+                $this->users = $this->users->andFilterWhere($model->getSearch());
+            }
+            if ($model->options && ArrayHelper::isIn(1,$model->options)) {
+                $this->users = $this->users->andFilterWhere($model->getFreeExecutors());
+            }
+            if ($model->options && ArrayHelper::isIn(2,$model->options)) {
+                $this->users = $this->users->andFilterWhere($model->getOnlineUsers());
+            }
+            if ($model->options && ArrayHelper::isIn(3,$model->options)) {
+                $this->users = $this->users->andFilterWhere($model->getUsersWithReviews());
+            }
+            if ($model->options && ArrayHelper::isIn(4,$model->options)) {
+                $this->users = $this->users->andFilterWhere($model->getFavouriteUsers());
+            }
+            if ($model->category_id) {
+            $this->users = $this->users->andFilterWhere($model->filterCategories());
+            }
+
+        }
+        $users = $this->users->all();
+
+        return $this->render('index', ['users' => $users, 'model' => $model, 'categories' => $categories]);
     }
 }
---
+++ frontend/models/FilterUsers.php
@@ -0,0 +1,85 @@
+<?php
+
+
+namespace frontend\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use yii\db\Expression;
+use yii\helpers\ArrayHelper;
+
+
+class FilterUsers extends Model
+{
+    public $category_id;
+    public $options;
+    public $search;
+
+    public function formName()
+    {
+        return '';
+    }
+
+    public function rules()
+    {
+        return [['category_id', 'safe'],
+            ['options', 'safe'],
+            ['search', 'safe']];
+    }
+
+    public function getOptions()
+    {
+        return [
+            1 => "Сейчас свободен",
+            2 => "Сейчас онлайн",
+            3 => "Есть отзывы",
+            4 => "В избранном"
+        ];
+    }
+
+
+    public function filterCategories()
+    {
+        if ($this->category_id) {
+            $id = [];
+            foreach ($this->category_id as $value) {
+                $id[] = $value;
+            }
+            return (['IN', 'executor_categories.category_id', $id]);
+        }
+    }
+
+    public function getOnlineUsers()
+    {
+        $now = new \DateTime('-30 minutes');
+        $now = $now->format('Y-m-d H:i:s');
+        return ['>=', 'dt_last_activity', $now];
+
+    }
+
+    public function getUsersWithReviews()
+    {
+        $users = Tasks::find()->select('executor_id')->joinWith('reviews', 'true', 'INNER JOIN')->asArray();
+        return ['IN', 'users.id', $users];
+    }
+
+    public function getFavouriteUsers()
+    {
+        $favouritesUsers = Favourites::find()->select('executor_id')->where(['author_id' => 3])->asArray();
+        return ['IN', 'users.id', $favouritesUsers];
+    }
+
+    public function getSearch()
+    {
+        return ['LIKE', 'name', $this->search];
+    }
+
+    public function getFreeExecutors()
+    {
+        $null = new Expression('NULL');
+        return ['IS', 'executor_id', $null];
+    }
+}
+
+
