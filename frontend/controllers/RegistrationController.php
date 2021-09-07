fc8c7ffc41da4a969cb258e7d35be3098574e379
Алексей <anonymous.aleksey@gmail.com>
2021-09-07 10:44:09 +0300

    done 6-3

--- frontend/controllers/RegistrationController.php
+++ frontend/controllers/RegistrationController.php
@@ -15,10 +15,12 @@ class RegistrationController extends Controller
      public function actionIndex() {
 
              $model = new Registration();
+             if (Yii::$app->request->post()) {
              $model->load(Yii::$app->request->post());
              if ($model->validate()) {
                  $model->signUp();
                  $this->goHome();
+             }
              }
          return $this->render('index', ['model' => $model]);
      }
