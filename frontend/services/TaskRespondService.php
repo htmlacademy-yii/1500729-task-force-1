882273a916fba50886910c74e423aace7400c06c
Алексей <anonymous.aleksey@gmail.com>
2021-11-12 21:47:46 +0300

    start 8.3

--- frontend/services/TaskRespondService.php
+++ frontend/services/TaskRespondService.php
@@ -2,8 +2,11 @@
 
 namespace frontend\services;
 
+use frontend\models\Notifications;
 use frontend\models\Responds;
+use frontend\models\Tasks;
 use Yii;
+use yii\db\Exception;
 use yii\web\BadRequestHttpException;
 
 class TaskRespondService
@@ -17,8 +20,21 @@ class TaskRespondService
             if ($model->validate()) {
                 $model->save();
             }
+            $this->sendNotification($task_id);
         } else {
             throw new BadRequestHttpException('Вы уже откликались на это задание');
+        }
+
+    }
+    private function sendNotification($task_id) {
+        $notification = new Notifications();
+        $notification->task_id = $task_id;
+        $notification->recipient = Tasks::findOne(['id' =>$task_id])->author_id;
+        $notification->type = 'respond';
+        if ($notification->validate()) {
+            $notification->save();
+        } else {
+            throw new Exception("Не удалось отправить уведомление");
         }
     }
 }
