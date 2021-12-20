d40c025ddc2f2a723ebd1a3e3ea2cb7ebd7c079a
Aleksey <anonymous.aleksey@gmail.com>
2021-12-20 12:53:02 +0300

    Oauth backURL

--- frontend/config/params.php
+++ frontend/config/params.php
@@ -5,5 +5,5 @@ return [
     'adminEmail' => 'admin@example.com',
     'yandexGeocoder' => 'e666f398-c983-4bde-8f14-e3fec900592a',
     'vk_id' => 8013503,
-    'vk_secret_key' => 'rJHNEKjVZ3VWUVlryzZB' //back URL установлен в значении taskforce.loc
+    'vk_secret_key' => 'rJHNEKjVZ3VWUVlryzZB' //back URL установлен в значении localhost
 ];
